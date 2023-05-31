b7d28de7b1175a5780f058f5ad9dde713f492dcc
lmachens <leon@machens.koeln>
2023-05-31 14:31:41 +0200

    Fix prerender

--- app/(overwolf)/app/page.tsx
+++ app/(overwolf)/app/page.tsx
@@ -1,15 +1,12 @@
 import dynamic from "next/dynamic";
 import Header from "../components/header";
 
-const Map = dynamic(() => import("../../[[...slug]]/components/(map)/map"), {
+const Map = dynamic(() => import("../../components/(map)/map"), {
   ssr: false,
 });
-const Tiles = dynamic(
-  () => import("../../[[...slug]]/components/(map)/tiles"),
-  {
-    ssr: false,
-  }
-);
+const Tiles = dynamic(() => import("../../components/(map)/tiles"), {
+  ssr: false,
+});
 
 export default async function App() {
   return (
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,1 @@
 export { default } from "@/app/(web)/page";
-
-export async function generateStaticParams() {
-  return [{ locale: "de" }];
-}
